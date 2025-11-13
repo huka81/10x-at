ce64980d66040abf426b0548f5bc8382ab9ea15e
Artur Huk <artur.huk.it@gmail.com>
2025-11-13 15:35:50 +0100

    Removed dead code

--- migrations/0110_create_user_config.sql
+++ migrations/0110_create_user_config.sql
@@ -13,8 +13,3 @@ CREATE TABLE IF NOT EXISTS at.users (
 CREATE INDEX IF NOT EXISTS idx_users_username ON at.users(username);
 CREATE INDEX IF NOT EXISTS idx_users_email ON at.users(email);
 CREATE INDEX IF NOT EXISTS idx_users_active ON at.users(is_active);
-
-INSERT INTO at.users (username, email, password_hash) 
-VALUES ('ahu', 'huk.artur@gmail.com', 'gAAAAABneXW1NRukIU3msbGjUGZCkg8vq57SQWMfqoo3eIXYI-W1Q_346H-WVHHj5llMXKo6scwc-KxtcUQVBOLEBOMPQuH-tQ==')
-ON CONFLICT (username) DO NOTHING;	 
-
